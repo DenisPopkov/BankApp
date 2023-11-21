c0cff18fed5b86548a89ebe97b61d558cd2ba75d
Denis Popkov <popkov-den03@mail.ru>
2023-11-21 10:11:15 +0600

    add: strings

---
+++ assets/string_assets.dart
@@ -0,0 +1,29 @@
+class StringAssets {
+  String userName = "Екатерина";
+  String profileTitle = "Профиль";
+  String settingsTitle = "Настройки";
+  String subscriptionTitle = "У вас подключено";
+  String subscriptionsTitle =
+      "Подписки, автоплатежи и сервисы на которые\nвы подписались";
+  String sberPrimeTitle = "СберПрайм";
+  String transactionsTitle = "Переводы";
+  String feeTitle = "Платёж 9 июля";
+  String feeSumTitle = "199 ₽ в месяц";
+  String autoSubscriptionTitle = "Автопродление 21 августа";
+  String limitsAndSubscriptionsTitle = "Тарифы и лимиты";
+  String onlySberTitle = "Для операций в Сбербанк Онлайн";
+  String transactionsAndFeeTitle = "На платежи и переводы";
+  String transactionsWithoutExtraTitle = "Переводы без комиссии";
+  String showBalanceTitle = "Показать остаток в этом месяце";
+  String limitInformationTitle = "Информация о тарифах\nи лимитах";
+  String interestTitle = "Интересы";
+  String historyCaption =
+      "Мы подбираем истории и предложения по темам,\nкоторые вам нравятся";
+  String foodChip = "Еда";
+  String selfLearnChip = "Саморазвитие";
+  String techChip = "Технологии";
+  String homeChip = "Дом";
+  String entertainmentChip = "Досуг";
+  String selfCareChip = "Забота о себе";
+  String scienceChip = "Наука";
+}
