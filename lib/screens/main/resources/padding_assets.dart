aa461b08d5f757d4273686e1cdaea887061c6c54
Denis Popkov <popkov-den03@mail.ru>
2023-11-21 11:33:41 +0600

    add: top bar widget

---
+++ lib/screens/main/resources/padding_assets.dart
@@ -0,0 +1,25 @@
+class PaddingAssets {
+  // Paddings
+  static double lightPadding = 4.0;
+  static double smallPadding = 8.0;
+  static double mediumPadding = 16.0;
+  static double bigPadding = 24.0;
+  static double headerPadding = 36.0;
+  static double bottomPadding = 36.0;
+  static double largePadding = 46.0;
+  static double superLargePadding = 58.0;
+
+  // Sizes
+  static double slightSize = 2.0;
+  static double mediumRadius = 12.0;
+  static double mediumSize = 12.0;
+  static double smallIcon = 24.0;
+  static double mediumIcon = 34.0;
+  static double imageSize = 110.0;
+  static double subscribeItemHeight = 130.0;
+  static double tarriffItemHeight = 54.0;
+  static double strokeWidth = 1.5;
+
+  // Spacings
+  static double smallSpacing = 8.0;
+}
